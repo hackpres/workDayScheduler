1e8d3a39a97fbcd05ffddae39a7683b39e8bac1b
hackpres <102390515+hackpres@users.noreply.github.com>
2022-05-25 15:55:10 -0400

    savebtns store textarea input to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,15 @@ const twoText = document.getElementById("2textarea");
 const threeText = document.getElementById("3textarea");
 const fourText = document.getElementById("4textarea");
 const fiveText = document.getElementById("5textarea");
+const saveBtn9 = document.getElementById("9btn");
+const saveBtn10 = document.getElementById("10btn");
+const saveBtn11 = document.getElementById("11btn");
+const saveBtn12 = document.getElementById("12btn");
+const saveBtn1 = document.getElementById("1btn");
+const saveBtn2 = document.getElementById("2btn");
+const saveBtn3 = document.getElementById("3btn");
+const saveBtn4 = document.getElementById("4btn");
+const saveBtn5 = document.getElementById("5btn");
     //sets the value of DateTime to luxon.DateTime so we dont have to type luxon. everytime we need DateTime.
         //its luxon syntax to include luxon. so this is for convenience.
 var DateTime = luxon.DateTime;
@@ -28,7 +37,10 @@ let currentTime = DateTime.now();
     //sets the value of currentTimeObject to the current time as an object, so I can easily get just the time measurement I need.
 let currentTimeObject = DateTime.now().toObject();
     //adds all the data-timeblockhour attributes for the table textarea's in an array
-let timeblockTime = [nineText.dataset.timeblockhour, tenText.dataset.timeblockhour, elevenText.dataset.timeblockhour, twelveText.dataset.timeblockhour, oneText.dataset.timeblockhour, twoText.dataset.timeblockhour, threeText.dataset.timeblockhour, fourText.dataset.timeblockhour, fiveText.dataset.timeblockhour]
+let timeblockTime = [nineText.dataset.timeblockhour, tenText.dataset.timeblockhour, elevenText.dataset.timeblockhour, twelveText.dataset.timeblockhour, oneText.dataset.timeblockhour, twoText.dataset.timeblockhour, threeText.dataset.timeblockhour, fourText.dataset.timeblockhour, fiveText.dataset.timeblockhour];
+    //adds all saveBtn elements into an array
+let saveBtnArray = [saveBtn9, saveBtn10, saveBtn11, saveBtn12, saveBtn1, saveBtn2, saveBtn3, saveBtn4, saveBtn5];
+
 
 
     //adds current Month Day, and Year to the currentDay element
@@ -55,6 +67,15 @@ function checkDateTimeVSTimeblock(hour, i) {
         tbElemLess.classList.remove("present");
     }
 };
+function addEventListenerOnBtns(tableBtn) {
+    tableBtn.addEventListener('click', () => {
+        localStorage.setItem(`CalendarItemFor ${tableBtn.id}`, `${tableBtn.parentElement.previousElementSibling.children[0].value}`);
+    })
+}
+
+// let btn9parent = saveBtn9.parentElement.previousElementSibling.children[0].value
+// console.log(btn9parent);
+
     //runs the checkDateTimeVSTimeblock function when the page loads to display the correct background color for the user
 timeblockTime.forEach((position) => checkDateTimeVSTimeblock(currentTimeObject.hour, position));
     //sets an interval every second to run the checkDateTimeVSTimeblock function for each timeblock textarea to change the background color so the user can see if the timeblock is past, present, or future.
@@ -63,5 +84,5 @@ let intervalID = setInterval(() => {
     timeblockTime.forEach((position) => checkDateTimeVSTimeblock(currentTimeObject.hour, position));
 }, 1000);
 
-
 //each save button should store the text content of the text area in the same tr to local storage
+saveBtnArray.forEach((index) => addEventListenerOnBtns(index));
